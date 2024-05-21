0f742779978bd6c5de8878753110c997e4acf043
shahul-github-s <shahul.github@gmail.com>
2024-05-21 22:57:13 +0530

    v0

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ form.addEventListener("submit", (e) => {
   fetch(scriptURL, { method: "POST", body: new FormData(dispositionForm) })
     .then((response) => console.log("Success!", response))
     .catch((error) => console.error("Error!", error.message));
+  alert("Thank you! your form is submitted successfully.");
 });
 
 document.addEventListener("DOMContentLoaded", (event) => {
